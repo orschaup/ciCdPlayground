04a987e41c9f41a1d70d04bdb6878b0d9b49c5e6
Paul <153192378+orschaup@users.noreply.github.com>
2024-05-03 15:36:48 +0200

    correct mistake

--- src/list.spec.js
+++ src/list.spec.js
@@ -13,7 +13,7 @@ describe('list', () => {
   it('shows content as paragraph if it is a string', () => {
     const {getByTestId} = render(List, {content: 'the line'})
     
-    const p = undefined // getByTestId('singleContentParagraph')
+    const p = getByTestId('singleContentParagraph')
     expect(p).toBeInTheDocument();
     expect(p).toHaveTextContent('the line');
   })
