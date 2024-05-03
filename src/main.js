1b734361e37351949fd28342c2c27022061ac82d
Paul <153192378+orschaup@users.noreply.github.com>
2024-05-03 13:39:19 +0200

    add personal infos

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Paul Orschau',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Rolling out new versions of software regularly'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'Github Actions',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'a little bit about Jenkins'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'go running', 
+			'PC gaming', 
+			'make cocktails'
 		]
 	}
 });
